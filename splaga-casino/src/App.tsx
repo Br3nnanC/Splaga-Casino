b54ab8be390107171f779c61e0d0f767f04a296f
Brennan Cox <br3nnancox@gmail.com>
2024-04-03 00:54:21 -0500

    The Beginning

---
+++ splaga-casino/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from './assets/splaga.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="App-logo">
+          <img src={Logo}/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
