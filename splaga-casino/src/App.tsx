4e846787fd6533a737f7a7eeca7990cd43eba6cb
Brennan Cox <br3nnancox@gmail.com>
2024-04-03 23:55:40 -0500

    Start of NavBar

--- splaga-casino/src/App.tsx
+++ splaga-casino/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Logo from './assets/splaga.png';
 import './App.css';
+import NavTabs from './components/navagationbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Nav-Bar">
         <div className="App-logo">
           <img src={Logo}/>
+        </div>
+        <div className='Tabs'>
+          <NavTabs/>
         </div>
     </div>
   );
